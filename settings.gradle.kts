5349846c43b25989b7735fdef991a15a4ea80335
geffaa <yodhimas02@gmail.com>
2023-11-02 08:29:01 +0700

    First Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PPAPB_Pertemuan10_RecyclerView"
+include(":app")
+ 
